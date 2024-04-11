5f42cc1b657e09fea80f121a77f65b0c08c83e88
alex <mmerenstein@cs.stonybrook.edu>
2024-04-11 14:34:38 +0000

    Make everything relative to script_dir

--- pull-images.sh
+++ pull-images.sh
@@ -5,7 +5,7 @@ source ${script_dir}/include.sh
 
 $DOCKERSUDO docker image pull ubuntu:22.04 &
 i=0
-for f in `find scripts/app-images/ -type f`; do
+for f in `find ${script_dir}/app-images/ -type f`; do
 	$DOCKERSUDO docker image pull $(cat $f) &
 	i=$((i+1))
 	if [ $i -gt 2 ]; then 
--- run-metrics-collection.sh
+++ run-metrics-collection.sh
@@ -12,25 +12,25 @@ chmod -R 777 output/${app}
 
 if [ -z "${SKIPRUN+x}" ]; then
 	extraargs=""
-	if [ -f scripts/app-extraargs/${app} ]; then
-		extraargs=$(cat scripts/app-extraargs/${app})
+	if [ -f ${script_dir}/app-extraargs/${app} ]; then
+		extraargs=$(cat ${script_dir}/app-extraargs/${app})
 	fi
-	$DOCKERSUDO docker run -d --rm --cap-add SYS_ADMIN -v ${CUDADIR}:${CONTAINER_CUDADIR} -v /opt/nvidia:/opt/nvidia --runtime=nvidia --gpus device=${device} -v /dev/shm:/dev/shm -v ${HOME}/output:/output -e app="${app}" -e gpu="${gpu}" -e MKL_SERVICE_FORCE_INTEL=1 -e MKL_THREADING_LAYER=GNU --name ${app} ${extraargs} $(cat scripts/app-images/${app}) tail -f /dev/null
+	$DOCKERSUDO docker run -d --rm --cap-add SYS_ADMIN -v ${CUDADIR}:${CONTAINER_CUDADIR} -v /opt/nvidia:/opt/nvidia --runtime=nvidia --gpus device=${device} -v /dev/shm:/dev/shm -v ${HOME}/output:/output -e app="${app}" -e gpu="${gpu}" -e MKL_SERVICE_FORCE_INTEL=1 -e MKL_THREADING_LAYER=GNU --name ${app} ${extraargs} $(cat ${script_dir}/app-images/${app}) tail -f /dev/null
 
 	read -p "Press enter to continue"
 fi
 
-cp scripts/app-cmds/${app} output/${app}/app-cmd.sh
+cp ${script_dir}/app-cmds/${app} output/${app}/app-cmd.sh
 
 $DOCKERSUDO docker exec ${app} ${CONTAINER_CUDADIR}/bin/nsys profile -f true -o /output/${app}/${gpu}.${app}.nsys_prof bash /output/${app}/app-cmd.sh
 read -p "Press enter to continue"
 
 $DOCKERSUDO docker exec ${app} ${CONTAINER_CUDADIR}/bin/nsys stats --report gpusum --force-overwrite true --force-export true --format json --output /output/${app}/${gpu} /output/${app}/${gpu}.${app}.nsys_prof.nsys-rep
 
-python3 scripts/parse-gpusum.py --in-file output/${app}/${gpu}_gpusum.json --out-dir /output/${app} --output-prefix ${gpu} --metrics-file scripts/metrics.json --app-cmdline "bash /output/${app}/app-cmd.sh" | sudo tee output/${app}/ncu-${app}.sh
+python3 ${script_dir}/parse-gpusum.py --in-file output/${app}/${gpu}_gpusum.json --out-dir /output/${app} --output-prefix ${gpu} --metrics-file ${script_dir}/metrics.json --app-cmdline "bash /output/${app}/app-cmd.sh" | sudo tee output/${app}/ncu-${app}.sh
 
 $DOCKERSUDO docker exec ${app} bash /output/${app}/ncu-${app}.sh 2>&1 | tee out
-./scripts/convert-ncu-output.sh output/${app}/
+${script_dir}/convert-ncu-output.sh output/${app}/
 
 read -p "Press enter to continue"
 docker stop ${app}
