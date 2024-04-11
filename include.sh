62421dbc67f932da55d155bb802b2f2785e33c3a
alex <mmerenstein@cs.stonybrook.edu>
2024-04-11 14:44:59 +0000

    Make waiting for user optional

--- include.sh
+++ include.sh
@@ -8,3 +8,10 @@ fi
 
 CUDADIR=${CUDADIR:-/usr/local/cuda-12.2}
 CONTAINER_CUDADIR=${CONTAINER_CUDADIR:-/usr/local/cuda-12.2}
+
+function wait_to_continue()
+{
+	if [ -f ${script_dir}/.WAIT_FOR_INPUT ]; then
+		read -p "Press enter to continue"
+	fi
+}
--- run-metrics-collection.sh
+++ run-metrics-collection.sh
@@ -17,20 +17,20 @@ if [ -z "${SKIPRUN+x}" ]; then
 	fi
 	$DOCKERSUDO docker run -d --rm --cap-add SYS_ADMIN -v ${CUDADIR}:${CONTAINER_CUDADIR} -v /opt/nvidia:/opt/nvidia --runtime=nvidia --gpus device=${device} -v /dev/shm:/dev/shm -v ${HOME}/output:/output -e app="${app}" -e gpu="${gpu}" -e MKL_SERVICE_FORCE_INTEL=1 -e MKL_THREADING_LAYER=GNU --name ${app} ${extraargs} $(cat ${script_dir}/app-images/${app}) tail -f /dev/null
 
-	read -p "Press enter to continue"
+	wait_to_continue
 fi
 
 cp ${script_dir}/app-cmds/${app} output/${app}/app-cmd.sh
 
 $DOCKERSUDO docker exec ${app} ${CONTAINER_CUDADIR}/bin/nsys profile -f true -o /output/${app}/${gpu}.${app}.nsys_prof bash /output/${app}/app-cmd.sh
-read -p "Press enter to continue"
+wait_to_continue
 
 $DOCKERSUDO docker exec ${app} ${CONTAINER_CUDADIR}/bin/nsys stats --report gpusum --force-overwrite true --force-export true --format json --output /output/${app}/${gpu} /output/${app}/${gpu}.${app}.nsys_prof.nsys-rep
 
 python3 ${script_dir}/parse-gpusum.py --in-file output/${app}/${gpu}_gpusum.json --out-dir /output/${app} --output-prefix ${gpu} --metrics-file ${script_dir}/metrics.json --app-cmdline "bash /output/${app}/app-cmd.sh" | sudo tee output/${app}/ncu-${app}.sh
 
-$DOCKERSUDO docker exec ${app} bash /output/${app}/ncu-${app}.sh 2>&1 | tee out
+$DOCKERSUDO docker exec ${app} bash /output/${app}/ncu-${app}.sh 2>&1 | tee output/${app}/ncu-out
 ${script_dir}/convert-ncu-output.sh output/${app}/
 
-read -p "Press enter to continue"
+wait_to_continue
 docker stop ${app}
